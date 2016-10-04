47f96cf8acb0175163bf5117b08dfbcfb2c5f187
susanthdahal <dahalsusanth@gmail.com>
2016-10-04 12:23:28 -0700

    Update KMP_algorithm.py

--- Arrays and Strings/KMP_algorithm.py
+++ Arrays and Strings/KMP_algorithm.py
@@ -3,25 +3,35 @@
 def compute_lps(pattern):
     j=0
     lps=[0]*len(pattern)
-    for i in range(1,len(pattern)):
-        while pattern[i]!=pattern[j] and j>0:
-            j=lps[j-1]
+    i=1
+    while i<len(pattern):
         if pattern[i]==pattern[j]:
             lps[i]=j+1
             j+=1
+            i += 1
         else:
-            lps[i]=0
+            if j!=0:
+                j=lps[j-1]
+            else:
+                lps[i]=0
+                j+=1
+                i+=1
     return lps
 
 def kmp(pattern,text,lps):
     j=0
-    for i in range(len(text)):
-        while j>0 and text[i]!=pattern[j]:
-            j=lps[j-1]
-        if text[i]==pattern[j]:
+    i=0
+    while i<len(text):
+        if pattern[j]==text[i]:
             j+=1
+            i+=1
+        else:
+            if j!=0:
+                j=lps[j-1]
+            else:
+                i+=1
         if j==len(pattern):
-            return i-(len(pattern)-1)
+            return i-len(pattern)
     return -1
 
 if __name__=='__main__':
